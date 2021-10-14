be2e68392b5a2eb4f0b708fd7d379679870cff7d
Taiyo Aragane <t.m.ny.0404@gmail.com>
2021-10-14 14:25:32 +0900

    コメント分修正

--- db/db-challenge2/create_table.sql
+++ db/db-challenge2/create_table.sql
@@ -9,21 +9,21 @@ CREATE TABLE members (
     phonenumber VARCHAR(13),
     cellphonenumber VARCHAR(13),
     created_at DATETIME not null,
-    update_at DATETIME not null,
-    is_deleted TINYINT(1) DEFAULT 0 
+    updated_at DATETIME not null,
+    is_deleted TINYINT(1) DEFAULT 0 not null
 )ENGINE=InnoDB , DEFAULT CHARSET=utf8;
 
 CREATE TABLE chatrooms(
     id INT PRIMARY KEY AUTO_INCREMENT,
     chatroom_name VARCHAR(100) not null,
     description VARCHAR(1000),
-    file_flag TINYINT(1) DEFAULT 1,
-    direct_flag TINYINT(1) DEFAULT 0,
+    is_file_sendable TINYINT(1) DEFAULT 1 not null,
+    is_direct_chat TINYINT(1) DEFAULT 0 not null,
     create_user_id INT not null ,
     created_at DATETIME not null,
     update_user_id INT not null ,
-    update_at DATETIME not null,
-    is_deleted TINYINT(1) DEFAULT 0,
+    updated_at DATETIME not null,
+    is_deleted TINYINT(1) DEFAULT 0 not null
     FOREIGN KEY (create_user_id) REFERENCES members(id),
     FOREIGN KEY (update_user_id) REFERENCES members(id)
 )ENGINE=InnoDB , DEFAULT CHARSET=utf8;
@@ -32,7 +32,7 @@ CREATE TABLE entries(
     id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT not null,
     chatroom_id INT not null ,
-    entried_at DATETIME not null,
+    created_at DATETIME not null,
     FOREIGN KEY(user_id) REFERENCES members(id),
     FOREIGN KEY(chatroom_id) REFERENCES chatrooms(id)
 )ENGINE=InnoDB , DEFAULT CHARSET=utf8;
@@ -43,10 +43,10 @@ CREATE TABLE chats(
     file_name VARCHAR(100),
     chat_user_id INT not null ,
     chatroom_id INT not null ,
-    chated_at DATETIME not null,
+    created_at DATETIME not null,
     update_user_id INT not null ,
-    update_at DATETIME,
-    is_deleted TINYINT(1) DEFAULT 0,
+    updated_at DATETIME not null,
+    is_deleted TINYINT(1) DEFAULT 0 not null,
     FOREIGN KEY (chat_user_id) REFERENCES members (id),
     FOREIGN KEY (chatroom_id) REFERENCES chatrooms(id),
     FOREIGN KEY (update_user_id) REFERENCES members (id)
@@ -62,8 +62,8 @@ CREATE TABLE tasks(
     create_user_id INT not null,
     created_at DATETIME not null,
     update_user_id INT not null,
-    update_at DATETIME not null,
-    is_deleted TINYINT(1) DEFAULT 0,
+    updated_at DATETIME not null,
+    is_deleted TINYINT(1) DEFAULT 0 not null,
     FOREIGN KEY (task_user_id) REFERENCES members(id),
     FOREIGN KEY (chatroom_id) REFERENCES chatrooms(id),
     FOREIGN KEY (create_user_id) REFERENCES members(id),
