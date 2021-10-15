e2bedf958b8bdc6b6fbc246049dfe06a7dcda1e7
Taiyo Aragane <t.m.ny.0404@gmail.com>
2021-10-15 19:08:24 +0900

    エラー修正分

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -1,9 +1,12 @@
-SELECT members.user_name , MAX(chats.created_at) , chatrooms.chatroom_name
-FROM members
-JOIN chats
-ON members.id = chats.chat_user_id
-JOIN chatrooms
-ON chats.chatroom_id = chatrooms.id
-WHERE members.is_deleted <> 1 AND chats.is_deleted <> 1
-GROUP BY chatrooms.chatroom_name
-ORDER BY chatrooms.id;
+SELECT members.user_name , chatrooms.chatroom_name , chatsA.created_at 
+FROM chats AS chatsA 
+JOIN (SELECT chatroom_id , MAX(created_at) AS 最新投稿
+FROM chats
+WHERE chats.is_deleted <> 1 
+GROUP BY chatroom_id) AS chatsB 
+ON chatsA.chatroom_id = chatsB.chatroom_id 
+AND chatsA.created_at = chatsB.最新投稿 
+JOIN members 
+ON chatsA.chat_user_id = members.id 
+JOIN chatrooms ON chatsA.chatroom_id = chatrooms.id 
+WHERE members.is_deleted <> 1 ORDER BY chatrooms.id;
